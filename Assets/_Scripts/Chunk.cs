e6cf1f892e229c797a0a4d400e454c95714fa4af
aznoqmous <paul.landreau931v@gmail.com>
2025-04-10 16:19:03 +0200

    multiple instance (chunks)

---
+++ Assets/_Scripts/Chunk.cs
@@ -0,0 +1,115 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField] private ComputeShader _computeShader;
+    [SerializeField] RawImage _renderImage;
+    RenderTexture _renderTexture;
+
+    int _size = 200;
+    int _scale = 4;
+
+    int _kernel;
+    Vector2Int _dispatchCount;
+    
+    ComputeBuffer _particlesBuffer;
+    ComputeBuffer _particleTypesBuffer;
+
+    [SerializeField] int _createdType = 1;
+
+    [SerializeField] List<ParticleResource> _particleTypes = new List<ParticleResource>();
+
+    void Start()
+    {
+
+        _computeShader = Instantiate(_computeShader);
+
+        uint threadX = 0;
+        uint threadY = 0;
+        uint threadZ = 0;
+        _kernel = _computeShader.FindKernel("CSMain");
+        _computeShader.GetKernelThreadGroupSizes(_kernel, out threadX, out threadY, out threadZ);
+        _dispatchCount.x = Mathf.CeilToInt(_size / threadX);
+        _dispatchCount.y = Mathf.CeilToInt(_size / threadY);
+
+        _renderTexture = new RenderTexture(_size, _size, 0, RenderTextureFormat.ARGBFloat);
+        _renderTexture.enableRandomWrite = true;
+        _renderTexture.wrapMode = TextureWrapMode.Clamp;
+        _renderTexture.filterMode = FilterMode.Point;
+        _renderTexture.Create();
+        _renderImage.texture = _renderTexture;
+        _renderImage.rectTransform.sizeDelta = new Vector2(_size, _size);
+
+        _computeShader.SetTexture(_kernel, "Result", _renderTexture);
+
+        _computeShader.SetInt("Size", _size);
+
+        InitParticles();
+    }
+
+    void InitParticles()
+    {
+        List<Particle> _particles = new List<Particle>();
+        for(int x = 0; x < _size; x++){
+            for(int y = 0; y < _size; y++){
+                Particle p = new Particle();
+                p.position = new Vector2(x, y);
+                _particles.Add(p);
+            }
+        }
+
+        int particleSize = sizeof(float) * 2
+            + sizeof(int)
+        ;
+        _particlesBuffer = new ComputeBuffer(Mathf.FloorToInt(Mathf.Pow(_size, 2f)), particleSize);
+        _particlesBuffer.SetData(_particles);
+        _computeShader.SetBuffer(_kernel, "Particles", _particlesBuffer);
+
+        int particleTypeSize = sizeof(float) * 4
+            + sizeof(int)
+        ;
+        List<ParticleType> particleTypes = new List<ParticleType>();
+        foreach(ParticleResource particleType in _particleTypes)
+        {
+            ParticleType p = new ParticleType();
+            p.color = particleType.color;
+            p.movementType = (int)particleType.movementType;
+            particleTypes.Add(p);
+        }
+        _particleTypesBuffer = new ComputeBuffer(_particleTypes.Count, particleTypeSize);
+        _particleTypesBuffer.SetData(particleTypes);
+        _computeShader.SetBuffer(_kernel, "Types", _particleTypesBuffer);
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = ((mousePosition - (Vector2)transform.position) / _scale / (_size / 200f) + Vector2.one) * _size / 2f;
+        _computeShader.SetVector("MousePosition", mousePosition);
+        _computeShader.SetBool("MouseDown", Input.GetMouseButton(0));
+        _computeShader.SetInt("MouseType", _createdType);
+        _computeShader.SetFloat("Time", Time.time);
+        _computeShader.Dispatch(_kernel, _dispatchCount.x, _dispatchCount.y, 1);
+    }
+
+    public void SetSize(int size){
+        _size = size;
+    }
+    public void SetScale(int scale){
+        _scale = scale;
+        transform.localScale = Vector3.one * scale;
+    }
+}
+
+public struct Particle {
+    public Vector2 position;
+    public int particleType;
+}
+
+public struct ParticleType 
+{
+    public Color color;
+    public int movementType;
+}
--- Assets/_Scripts/Simulation.cs
+++ Assets/_Scripts/Simulation.cs
@@ -1,107 +1,25 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Simulation : MonoBehaviour
 {
-    [SerializeField] private ComputeShader _computeShader;
-    [SerializeField] RawImage _renderImage;
-    RenderTexture _renderTexture;
+    [SerializeField] Chunk _chunkPrefab;
+    [SerializeField] Dictionary<Vector2Int, Chunk> _chunks = new Dictionary<Vector2Int, Chunk>();
 
-    [SerializeField] int _size = 200;
-    int _kernel;
-    Vector2Int _dispatchCount;
-    
-    ComputeBuffer _particlesBuffer;
-    ComputeBuffer _particleTypesBuffer;
-
-    [SerializeField] int _createdType = 1;
-    [SerializeField] Color _createdColor = Color.white;
-
-    [SerializeField] List<ParticleResource> _particleTypes = new List<ParticleResource>();
+    [SerializeField] int _chunkSize = 200;
+    [SerializeField] int _chunkScale = 4;
 
     void Start()
     {
-        uint threadX = 0;
-        uint threadY = 0;
-        uint threadZ = 0;
-        _kernel = _computeShader.FindKernel("CSMain");
-        _computeShader.GetKernelThreadGroupSizes(_kernel, out threadX, out threadY, out threadZ);
-        _dispatchCount.x = Mathf.CeilToInt(_size / threadX);
-        _dispatchCount.y = Mathf.CeilToInt(_size / threadY);
-
-        _renderTexture = new RenderTexture(_size, _size, 0, RenderTextureFormat.ARGBFloat);
-        _renderTexture.enableRandomWrite = true;
-        _renderTexture.wrapMode = TextureWrapMode.Clamp;
-        _renderTexture.filterMode = FilterMode.Point;
-        _renderTexture.Create();
-        _renderImage.texture = _renderTexture;
-        _renderImage.rectTransform.sizeDelta = new Vector2(_size, _size);
-
-        _computeShader.SetTexture(_kernel, "Result", _renderTexture);
-
-        _computeShader.SetInt("Size", _size);
-
-        InitParticles();
-    }
-
-    void InitParticles()
-    {
-        List<Particle> _particles = new List<Particle>();
-        for(int x = 0; x < _size; x++){
-            for(int y = 0; y < _size; y++){
-                Particle p = new Particle();
-                /*if(UnityEngine.Random.value < 0.1f) p = sandParticle;
-                else if(UnityEngine.Random.value < 0.1f) p = waterParticle;*/
-                p.position = new Vector2(x, y);
-                _particles.Add(p);
+        Debug.Log(Screen.width + " " + Screen.height);
+        for(int x = 0; x < Screen.width / (_chunkSize * _chunkScale) + 1; x++){
+            for(int y = 0; y < Screen.height / (_chunkSize * _chunkScale) + 1; y++){
+                Vector3 position = new Vector3(x * _chunkSize / 100f * _chunkScale, y * _chunkSize / 100f * _chunkScale);
+                Chunk newChunk = Instantiate(_chunkPrefab, position, Quaternion.identity, transform);
+                newChunk.SetSize(_chunkSize);
+                newChunk.SetScale(_chunkScale);
             }
-        }
-
-        int particleSize = sizeof(float) * 2
-            + sizeof(int)
-        ;
-        _particlesBuffer = new ComputeBuffer(Mathf.FloorToInt(Mathf.Pow(_size, 2f)), particleSize);
-        _particlesBuffer.SetData(_particles);
-        _computeShader.SetBuffer(_kernel, "Particles", _particlesBuffer);
-
-        int particleTypeSize = sizeof(float) * 4
-            + sizeof(int)
-        ;
-        List<ParticleType> particleTypes = new List<ParticleType>();
-        foreach(ParticleResource particleType in _particleTypes)
-        {
-            ParticleType p = new ParticleType();
-            p.color = particleType.color;
-            p.movementType = (int)particleType.movementType;
-            particleTypes.Add(p);
-        }
-        _particleTypesBuffer = new ComputeBuffer(_particleTypes.Count, particleTypeSize);
-        _particleTypesBuffer.SetData(particleTypes);
-        _computeShader.SetBuffer(_kernel, "Types", _particleTypesBuffer);
-    }
-
-    void Update()
-    {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition = (mousePosition / transform.localScale.x / (_size / 200f) + Vector2.one) * _size / 2f;
-        _computeShader.SetVector("MousePosition", mousePosition);
-        _computeShader.SetBool("MouseDown", Input.GetMouseButton(0));
-        _computeShader.SetVector("MouseColor", _createdColor);
-        _computeShader.SetInt("MouseType", _createdType);
-        _computeShader.SetFloat("Time", Time.time);
-        _computeShader.Dispatch(_kernel, _dispatchCount.x, _dispatchCount.y, 1);
+        }   
     }
 }
 
-public struct Particle {
-    public Vector2 position;
-    public int particleType;
-}
-
-public struct ParticleType 
-{
-    public Color color;
-    public int movementType;
-}
