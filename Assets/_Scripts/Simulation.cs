42b2e508d3f931e0319124640a8320cfe6b9a7b8
aznoqmous <paul.landreau931v@gmail.com>
2025-04-10 16:26:40 +0200

    simulation fill screen

--- Assets/_Scripts/Simulation.cs
+++ Assets/_Scripts/Simulation.cs
@@ -11,10 +11,15 @@ public class Simulation : MonoBehaviour
 
     void Start()
     {
-        Debug.Log(Screen.width + " " + Screen.height);
-        for(int x = 0; x < Screen.width / (_chunkSize * _chunkScale) + 1; x++){
-            for(int y = 0; y < Screen.height / (_chunkSize * _chunkScale) + 1; y++){
-                Vector3 position = new Vector3(x * _chunkSize / 100f * _chunkScale, y * _chunkSize / 100f * _chunkScale);
+        Vector2 screenChunks = new Vector2(
+            Screen.width / (float)(_chunkSize * _chunkScale) + 1f,
+            Screen.height / (float)(_chunkSize * _chunkScale) + 1f
+        );
+        float worldChunkSize = _chunkSize / 100f * _chunkScale;
+        Debug.Log(screenChunks);
+        for(int x = 0; x < screenChunks.x; x++){
+            for(int y = 0; y < screenChunks.y; y++){
+                Vector3 position = new Vector3((x - screenChunks.x / 2f) * worldChunkSize, (y - screenChunks.y / 2f) * worldChunkSize);
                 Chunk newChunk = Instantiate(_chunkPrefab, position, Quaternion.identity, transform);
                 newChunk.SetSize(_chunkSize);
                 newChunk.SetScale(_chunkScale);
