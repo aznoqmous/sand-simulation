3030b6d74c487d1d438199e689e97b966eb9fb5c
aznoqmous <paul.landreau931v@gmail.com>
2025-04-11 12:11:06 +0200

    infinite chunk generation based on player position

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _scrollSpeed = 1f;
-    
+    [SerializeField] float _moveSpeed = 10f;
+
     void Start()
     {
         
@@ -13,6 +14,6 @@ public class Player : MonoBehaviour
     {
         Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z), Time.deltaTime * 2f);
         Camera.main.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * _scrollSpeed;
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * 5f;
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * _moveSpeed;
     }
 }
--- Assets/_Scripts/Simulation.cs
+++ Assets/_Scripts/Simulation.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Simulation : MonoBehaviour
 {
@@ -16,45 +17,76 @@ public class Simulation : MonoBehaviour
 
     [SerializeField] Player player;
 
+    float _worldChunkSize = 0f;
+
     public int CreatedType => _createdType;
     public List<ParticleResource> ParticleTypes => _particleTypes;
     public bool DrawBounds => _drawBounds;
 
     void Start()
     {
-        Vector2 screenChunks = new Vector2(
-            Screen.width / (float)(_chunkSize * _chunkScale) + 2f,
-            Screen.height / (float)(_chunkSize * _chunkScale) + 2f
+        float ratio = 1f / 0.0193f;
+        _worldChunkSize = _chunkSize / ratio * _chunkScale;
+
+        UpdateChunks();
+    }
+
+    Vector2Int GetScreenChunks()
+    {
+        Vector3 chunkSize = Camera.main.WorldToScreenPoint(new Vector3(_worldChunkSize, _worldChunkSize, 0));
+        return new Vector2Int(
+            Mathf.CeilToInt(Screen.width / chunkSize.x),
+            Mathf.CeilToInt(Screen.height / chunkSize.y)
+        );
+    }
+
+    Vector2Int GetActiveChunks()
+    {
+        return new Vector2Int(
+            _activeChunkDistance + 1,
+            _activeChunkDistance + 1
         );
 
-        Debug.Log(Screen.width + " " + Screen.height + " " + screenChunks.x + " " + screenChunks.y);
+    }
 
-        float ratio = 1f / 0.0193f;
-
-        float worldChunkSize = _chunkSize / ratio * _chunkScale;
-        for(int x = 0; x < screenChunks.x; x++){
-            for(int y = 0; y < screenChunks.y; y++){
-                Vector3 position = new Vector3((x - screenChunks.x / 2f) * worldChunkSize, (y - screenChunks.y / 2f) * worldChunkSize);
-                Chunk newChunk = Instantiate(_chunkPrefab, position, Quaternion.identity, transform);
-                newChunk.SetSize(_chunkSize);
-                newChunk.SetScale(_chunkScale);
-                newChunk.Init(this);
-                newChunk.name = $"Chunk {x} {y}";
-                AddChunk(new Vector2Int(x, y), newChunk);
+    void UpdateChunks(){
+        Vector3 playerPosition = player.transform.position / _worldChunkSize;
+        Vector2Int activeChunks = GetActiveChunks();
+        for(int x = Mathf.FloorToInt(playerPosition.x) - activeChunks.x; x <= activeChunks.x; x++)
+        {
+            for (int y = Mathf.FloorToInt(playerPosition.y) - activeChunks.y; y <= activeChunks.y; y++)
+            {
+                Vector2Int position = new Vector2Int(x, y);
+                if (!_chunks.ContainsKey(position))
+                {
+                    Chunk newChunk = CreateChunk(position);
+                    AddChunk(position, newChunk);
+                }
             }
         }
     }
-
     void Update()
     {
+        UpdateChunks();
         foreach(Chunk chunk in _chunks.Values)
         {
-            chunk.gameObject.SetActive((player.transform.position - chunk.transform.position).magnitude < _activeChunkDistance);
+            chunk.gameObject.SetActive((player.transform.position - chunk.transform.position).magnitude < _activeChunkDistance * _worldChunkSize);
         }
+    }
+
+    public Chunk CreateChunk(Vector2Int chunkPosition)
+    {        
+        Chunk chunk = Instantiate(_chunkPrefab, new Vector3(chunkPosition.x * _worldChunkSize, chunkPosition.y * _worldChunkSize), Quaternion.identity, transform);
+        return chunk;
     }
 
     public void AddChunk(Vector2Int position, Chunk chunk)
     {
+        chunk.SetSize(_chunkSize);
+        chunk.SetScale(_chunkScale);
+        chunk.Init(this);
+        chunk.name = $"Chunk {position.x} {position.y}";
+
         if (_chunks.ContainsKey(position))
         {
             Debug.LogWarning($"Chunk at {position} already exists. Overwriting.");
